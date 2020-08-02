d2e59ea036a741495aac8153f7eb7702971e6769
Kimaru thagana <macbuntu@192.168.1.8>
2020-08-02 21:08:22 +0300

    add payloads

--- doctors/resolvers/mutations/doctors.py
+++ doctors/resolvers/mutations/doctors.py
@@ -1,7 +1,8 @@
-from typing import Dict, Optional
+from typing import Dict
 
 from ariadne import convert_kwargs_to_snake_case
 from doctors.models import Doctor
+from django.db import transaction
 
 class DoctorsMutations:
     """
@@ -13,22 +14,47 @@ class DoctorsMutations:
 
     @staticmethod
     @convert_kwargs_to_snake_case
-    def create(_, info, create_input: Dict):
+    def create(_, info, create_input):
 
         try:
+            with transaction.atomic():
 
-
-            return dict(
+                return dict(
                 status=True,
                 object=Doctor.objects.create(create_input)
-            )
+                )
         except Exception as e:
             return dict(status=False, error=f'An error occurred {e}')
 
     @staticmethod
     @convert_kwargs_to_snake_case
-    def soft_delete(_, info, uid: str, deleted_by: Optional[str] = None):
+    def update(_, info, update_input):
+
+        try:
+            with transaction.atomic():
+
+                return dict(
+                    status=True,
+                    object=Doctor.objects.update(update_input)
+                )
+        except Exception as e:
+            return dict(status=False, error=f'An error occurred {e}')
+
+
+    @staticmethod
+    @convert_kwargs_to_snake_case
+    def soft_delete(_, info, uid, deleted_by = None):
         if deleted_by:
-            return MutationUtils.soft_delete(
-                info=info, model=Doctor, uid=uid, deleted_by=deleted_by
-            )
+            try:
+                with transaction.atomic():
+                    record = Doctor.objects.get(uid=uid)
+                    record.soft_delete(deleted_by)
+                    return dict(
+                        status=True,
+                        object=record
+                    )
+            except Exception as e:
+                return dict(
+                    status=False,
+                    error=f'An error occurred: {e}'
+                )
--- doctors/resolvers/queries/doctors.py
+++ doctors/resolvers/queries/doctors.py
@@ -3,7 +3,7 @@ from typing import Any, Dict, Optional
 from ariadne import convert_kwargs_to_snake_case
 
 from doctors.models import Doctor
-from utils.queries import QueryUtils
+from django.db import transaction
 
 
 class DoctorsQueries:
@@ -12,15 +12,32 @@ class DoctorsQueries:
     """
 
     @staticmethod
-    @convert_kwargs_to_snake_case
-    def get_doctor(_, info, uid: str) -> Dict:
-
-        return QueryUtils.get_single_object(info=info, model=Doctor, uid=uid)
+    def filter(queryset, filter_input):
+        return queryset.filter(**filter_input)
 
     @staticmethod
     @convert_kwargs_to_snake_case
-    def get_doctors(_, info, pagination_input: Optional[Dict[Any, Any]] = None,
-                       filter_input: Optional[Dict[Any, Any]] = None) -> Dict:
+    def get_doctor(_, info, uid):
 
-        return QueryUtils.get_multiple_objects(info=info, model=Doctor, filter_input=filter_input,
-                                               pagination_input=pagination_input)
+        try:
+            with transaction.atomic():
+                return dict(
+                    status=True,
+                    object=Doctor.objects.filter(uid=uid).not_deleted()[0]
+                )
+        except Exception as e:
+            return dict(
+                status=False,
+                error=f'An error as occurred {e}'
+            )
+
+    @staticmethod
+    @convert_kwargs_to_snake_case
+    def get_doctors(_, info, pagination_input,filter_input):
+
+        try:
+            return paginate (DoctorsQueries.filter(
+                Doctor.objects.all().not_deleted(), filter_input=filter_input),
+                            pagination_input=pagination_input)
+        except Exception as e:
+            return dict(status=False, error=f"An error as occurred {e}")
