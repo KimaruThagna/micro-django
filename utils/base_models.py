e19c0dbd3ed538762b5a96f2172828ee4507904d
Kimaru thagana <macbuntu@192.168.1.8>
2020-08-02 21:40:29 +0300

    add mutation and queries

--- utils/base_models.py
+++ utils/base_models.py
@@ -41,25 +41,18 @@ class BaseModel(models.Model):
     # uuid4 field for retrieval, this field should also be indexed
     uid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
-    created_by = models.UUIDField(blank=True, null=True)
-
     is_deleted = models.BooleanField(default=False)
     deleted_at = models.DateTimeField(null=True)
-    deleted_by = models.UUIDField(blank=True, null=True)
     is_active = models.BooleanField(default=True)
 
 
     class Meta:
         abstract = True
 
-    def soft_delete(self, deleted_by: uuid.UUID):
-        """
-        :param deleted_by: user performing deletion
-        perform deletion by setting is_deleted flag to True
-        """
+    def soft_delete(self):
+
         self.is_deleted = True
         self.deleted_at = timezone.now()
-        self.deleted_by = deleted_by
         self.save()
 
     def activate(self):
