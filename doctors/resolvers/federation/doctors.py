11d07280023abcf3206301fd2e9d8f4b89a46a53
Kimaru thagana <macbuntu@localhost.localdomain>
2020-08-12 13:39:12 +0300

    add federation object type refernce

---
+++ doctors/resolvers/federation/doctors.py
@@ -0,0 +1,41 @@
+from ariadne.contrib.federation import FederatedObjectType
+
+from doctors.models import Doctor
+
+doctor_federated_object = FederatedObjectType("Doctor")
+
+
+@doctor_federated_object.resolve_reference
+def get_doctor_by_uid(representation):
+    return Doctor.objects.get(uid=representation.get("uid"))
+
+
+@doctor_federated_object.field("uid")
+def resolve_id(obj, *_):
+    return obj.uid
+
+
+@doctor_federated_object.field("indexingId")
+def resolve_indexing_id(obj, *_):
+    return obj.indexing_id
+
+
+@doctor_federated_object.field("first_name")
+def resolve_first_name(obj, *_):
+    return obj.first_name
+
+@doctor_federated_object.field("last_name")
+def resolve_last_name(obj, *_):
+    return obj.last_name
+
+@doctor_federated_object.field("specialization")
+def resolve_specialization(obj, *_):
+    return obj.specialization
+
+@doctor_federated_object.field("license_number")
+def resolve_license_number(obj, *_):
+    return obj.license_number
+
+@doctor_federated_object.field("county")
+def resolve_county(obj, *_):
+    return obj.county
